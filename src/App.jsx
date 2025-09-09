f773adbbea24474116f86d2c03c915d49c31c1ff
Onur DÖKER <odoker@msn.com>
2025-09-09 17:16:26 +0300

    initial upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ContactForm from "./components/ContactForm/ContactForm";
+
+function App() {
+  
+  return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm />
+      
+      
+      </div>
+  );
+}
+
+export default App;
