16d5b9d068b724363eb1911a201078750bdcdeef
Onur DÖKER <odoker@msn.com>
2025-09-10 23:48:51 +0300

    Numeric and special character validation was added during the search

--- src/components/searchbox/SearchBox.jsx
+++ src/components/searchbox/SearchBox.jsx
@@ -1,13 +1,21 @@
 import css from "./SearchBox.module.css";
 import { useId } from "react";
+import { useState } from "react";
 
 const SearchBox = ({ handleSearchChange }) => {
   
-  const handleSearchSubmit = (event) => {
-    handleSearchChange(event.currentTarget.value);
-  };
+  const [error, setError] = useState("");
   
-  //TODO => Check & control entering empty string or digit entries
+  const handleSearchSubmit = (event) => {
+    const value = event.currentTarget.value;
+    if (value === "" || /^[A-Za-z\s]+$/.test(value)) {
+      handleSearchChange(event.currentTarget.value);
+      setError("");
+    } else {
+      setError("Search can only contain letters and spaces");
+    }
+    
+  };
   
   const nameId = useId();
   
@@ -22,6 +30,7 @@ const SearchBox = ({ handleSearchChange }) => {
             onChange={handleSearchSubmit}
             id={nameId}
         />
+        {error && <span style={{ color: "red" }}>{error}</span>}
       </div>
   );
 };
